159605673c2a70cf5296e352ccbded89f9459de0
Kvn-OG <keven.oliveira2k22@gmail.com>
2024-07-12 23:00:49 -0300

    OpenWeather API integration and made some changes on the HTML document and CSS styles, and now the application returns the data at the screen

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 const key = "400d432abb66e77d9939c4d79c6144e1"
 
+function showData(data){
+    document.querySelector(".city").innerHTML = data.name
+    document.querySelector(".temperature").innerHTML = Math.floor(data.main.temp) + '°C'
+    document.querySelector(".description").innerHTML = data.weather[0].description
+    document.querySelector(".humidity").innerHTML = "Umidade " + data.main.humidity + '%'
+    document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h"
+    document.querySelector(".weather-icon").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+}
+
 async function citySearch(city){
     const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pt_br&units=metric`).then(result => result.json())
 
-    console.log(data)
+    showData(data)
 }
 
 function buttonClick(){
